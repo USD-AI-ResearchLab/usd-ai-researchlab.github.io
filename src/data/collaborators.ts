fdd188192c64d44f0637a3c2e9cb693e6510ee47
Deepika Nuthalapati <Deepika.Nuthalapati@coyotes.usd.edu>
2026-02-13 14:11:14 -0600

    Fix Google Scholar placeholder URLs causing errors
    - Commented out invalid Google Scholar URLs with YOUR_ID_HERE placeholders
    - Added TODO comments for future addition of actual Scholar profiles
    - Prevents broken links and navigation errors

--- src/data/collaborators.ts
+++ src/data/collaborators.ts
@@ -13,7 +13,7 @@ export const collaboratorsData: Person[] = [
     name: "Dr. Veerpratap Meena",
     role: "Assistant Professor\nDepartment of Electrical Engineering\nNational Institute of Technology\nJamshedpur, India",
     url: "https://www.linkedin.com/in/veerpratapmeena/",
-    scholarUrl: "https://scholar.google.com/citations?user=YOUR_ID_HERE", // Will need to be updated with actual Google Scholar profile
+    // scholarUrl: "https://scholar.google.com/citations?user=YOUR_ID_HERE", // TODO: Add actual Google Scholar profile
     // Based on ResearchGate profile: 101+ publications, Associate Editor for IEEE Access, 
     // General Chair for multiple IEEE conferences, 2% Scientists 2024 recognition
     // Research focus: Machine Learning, Computer Vision, Signal Processing
@@ -22,7 +22,7 @@ export const collaboratorsData: Person[] = [
     name: "Akram Bennour, PhD",
     role: "Professor of Computer Science\nLarbi Tebessi University\nLAMIS Laboratory\nAlgeria",
     url: "https://sites.google.com/view/akram-bennour/biography?authuser=0",
-    scholarUrl: "https://scholar.google.com/citations?user=YOUR_ID_HERE", // Will need to be updated with actual Google Scholar profile
+    // scholarUrl: "https://scholar.google.com/citations?user=YOUR_ID_HERE", // TODO: Add actual Google Scholar profile
     // Research collaborator from Université de Tebessa, Algeria
     // Research focus: AI, Machine Learning, Image Processing, Pattern Recognition, Medical Image Analysis
     // General Chair for ISPR conferences, Editorial roles in multiple journals
